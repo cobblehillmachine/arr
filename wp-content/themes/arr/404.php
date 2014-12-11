86b05dff95e317d45cfdb411dbbc202d71d82752
Trevor <tmwagner66@gmail.com>
2014-12-11 13:47:20 -0500

    fixing 4040 pge

--- wp-content/themes/arr/404.php
+++ wp-content/themes/arr/404.php
@@ -9,18 +9,16 @@
 
 get_header(); ?>
 
+<div class="single-service">
 	<div class="mid-cont">
-
 		<header class="page-header">
 			<h1 class="page-title"><?php _e( 'Not Found', 'twentyfourteen' ); ?></h1>
 		</header>
-
 		<div class="page-content">
 			<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'twentyfourteen' ); ?></p>
-
 			<?php get_search_form(); ?>
 		</div>
-
 	</div>
+</div>
 
 <?php get_footer(); ?>
