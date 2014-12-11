b9423db9ff8ba29a1e9c26a77fb2d8243dc2b3a8
Trevor <tmwagner66@gmail.com>
2014-12-11 15:22:12 -0500

    WHERE IS THIS BUG< DAMMIT????

--- wp-content/themes/arr/page-services.php
+++ wp-content/themes/arr/page-services.php
@@ -1,4 +1,14 @@
-<?php get_header(); ?>
+<?php
+/**
+* Template Name: Template - Services
+* Description: Home page Template
+*
+* @package WordPress
+* @subpackage Twenty_Fourteen
+* @since Twenty Fourteen 1.0
+*/
+
+get_header(); ?>
 
 <!--
 
